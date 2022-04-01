bf79d1f283598582b7aa6e336b6c84e0c1a9493c
Danny Munoz <danic0045@gmail.com>
2022-04-01 12:42:59 -0500

    Publicacion Inicial

---
+++ src/test/java/com/movimientoCuentas/demo/CuentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movimientoCuentas.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
